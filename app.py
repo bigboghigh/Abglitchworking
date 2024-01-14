40414ae42f521f2e8185bee186670287943ca3b7
bigboghigh <156019142+bigboghigh@users.noreply.github.com>
2024-01-14 14:40:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, jsonify
 import requests
 import time
 import json
 import threading
 
 app = Flask(__name__)
-
 
 @app.route('/add/coins.php', methods=['GET', 'POST', 'OPTIONS'])
 def handle_request():
@@ -20,12 +19,27 @@ def handle_request():
     if request.method == 'POST':
         key = request.json.get('key')
         token = request.json.get('token')
-        number = request.json.get('number')
-        oldbal = request.json.get('oldbal')  # Get old balance from frontend
-        userid = request.json.get('userid')  # Get userid from frontend
+        number = request.json.get('number') 
+        oldbal = request.json.get('oldbal') 
+        userid = request.json.get('userid') 
 
-        if key is None or token is None or number is None or oldbal is None or userid is None:
-            return jsonify({"error": "Missing parameters"}), 400
+        missing_parameters = []
+
+        # Check for missing parameters
+        if key is None:
+            missing_parameters.append("key")
+        if token is None:
+            missing_parameters.append("token")
+        if number is None:
+            missing_parameters.append("number")
+        if oldbal is None:
+            missing_parameters.append("oldbal")
+        if userid is None:
+            missing_parameters.append("userid")
+
+        if missing_parameters:
+            error_message = "Missing parameters: {}".format(', '.join(missing_parameters))
+            return jsonify({"error": error_message}), 400
 
         try:
             number = int(number)
@@ -34,25 +48,20 @@ def handle_request():
             return jsonify({"error": "Invalid number parameter"}), 400
 
         def send_requests(key, token, number, oldbal, userid):
-            url = 'http://abtalkapi.oneway-tech.com/credits/v3/ad/reward'
+            url = 'https://abtalk-by-romeo.vercel.app/activate?key={}&deviceid={}'.format(key, token)
 
             headers = {
-                'version': '167465',
-                'token': token,
-                'deviceid': key,
-                'content-type': 'application/json; charset=UTF-8',
+                'Host': 'abtalk-by-romeo.vercel.app'
             }
 
-            data = {
-                "requestTime": 1702555980794,
-                "sign": "TKyQ1b8oydBSd8wPxhUqFM4Vcc2IIXHT1PePm55ms1CvU3UvDtMHDS0d2OUHg/QN9u6vLd09qW1sQPiWfc6PjOVNkAyvJGp95S8t8TLDBJeQCgYXadkWJRjk0yPLeQE59hByKlIifzDMwCzcKzifG5m+WPLzIhW5kLYzcm+Y0g96gougnjpC3SSvEkDg9GiyQYeYoqTZ42nKZj2tkVb4fX8xP3Za2qnNU4gyhBQTOnVUlohqm2FZQrdfj7D+D0nndXtnZJ9XUlY0mChpBqzWbWx6ymqjBrjSseuWfzKOgmRwpeL0PzCwevgkWM3qeyJ/R8btUt/FR9XliSFt0FP2hw==",
-                "creditAmount": 999,
-                "projectId": "DT_2022102902",
-            }
+            
 
             while True:
                 try:
-                    response = requests.post(url, json=data, headers=headers)
+                    response = requests.get(url=url, headers=headers)
+                    print("API Response: {}".format(response.text))
+                    print("API Status Code: {}".format(response.status_code))
+                    response.raise_for_status()  # Raise an HTTPError for bad responses
                     response_json = response.json()
                     credit_amount = response_json.get('data', {}).get('creditsAmount')
                     if credit_amount is not None:
@@ -62,8 +71,21 @@ def handle_request():
                         elif credit_amount >= oldbal + 999:
                             send_put_request(userid)  # Send PUT request when credit_amount increases by 1000
                             oldbal = oldbal + 999  # Update oldbal when increased by 1000
-                except Exception as e:
+
+                        # Log the response and status code
+                        print("API Response: {}".format(response.text))
+                        print("API Status Code: {}".format(response.status_code))
+
+                except requests.exceptions.HTTPError as e:
+                    # Log the error response status code
+                    print("API Error Response Status Code: {}".format(response.status_code)) 
+                    print("Api text: {}".format(response.text))  
                     break
+                except Exception as e:
+                    # Log the exception
+                    print("Error in API request: {}".format(e))
+                    break
+
                 time.sleep(10)
 
         # Start the thread for continuous requests
@@ -71,16 +93,21 @@ def handle_request():
         thread.start()
 
         # Return initial response 'done'
-        return jsonify({"message": "done"})
+        return jsonify({"message": "done"}) 
 
     return app.send_static_file('index.html')
 
 def send_put_request(userid):
-    url = "https://juicy-trapezoidal-wire.glitch.me/deductUserbalance/" + str(userid) 
+    url = "https://juicy-trapezoidal-wire.glitch.me/deductUserbalance/" + str(userid)
     headers = {"content-type": "application/json"}
     payload = {"amountToDeduct": "1"}
 
     response = requests.put(url, json=payload, headers=headers)
+
+    # Log the response and status code
+    print("PUT Request Response: {}".format(response.text))
+    print("PUT Request Status Code: {}".format(response.status_code))
+
     return response
 
 if __name__ == '__main__':
